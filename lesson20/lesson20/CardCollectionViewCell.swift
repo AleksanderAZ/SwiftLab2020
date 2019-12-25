6ed9b209fdf0a7caa96f43a5e305fb7a454ba8ac
Aleksander <aleksanderaz@gmail.com>
2019-12-25 17:39:04 +0200

    lesson 25

--- lesson20/lesson20/CardCollectionViewCell.swift
+++ lesson20/lesson20/CardCollectionViewCell.swift
@@ -13,5 +13,27 @@ class CardCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var frontImageView: UIImageView!
     
     @IBOutlet weak var backImageView: UIImageView!
+   
+    var card: Card?
     
+    func setCard(_ card: Card) {
+        
+        self.card = card
+        frontImageView.image = UIImage(named: card.imageName)
+        
+        if card.isFlipped {
+            UIView.transition(from: backImageView, to: frontImageView, duration: 0.3, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+        }
+        else {
+            UIView.transition(from: frontImageView, to: backImageView, duration: 0.3, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+        }
+    }
+    
+    func flip() {
+        UIView.transition(from: backImageView, to: frontImageView, duration: 0.3, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+    }
+    
+    func flipback() {
+        UIView.transition(from: frontImageView, to: backImageView, duration: 0.3, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+    }
 }
--- lesson20/lesson20/ViewController.swift
+++ lesson20/lesson20/ViewController.swift
@@ -13,12 +13,51 @@ class ViewController: UIViewController {
     var model = CarDModel()
     var cardArray = [Card]()
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        collectionView.delegate = self
+        collectionView.dataSource = self
         cardArray = model.getCards()
     }
 
 
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cardArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCollectionViewCell", for: indexPath)
+        if let cell = cell as? CardCollectionViewCell {
+            cell.setCard(cardArray[indexPath.row])
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let cell = collectionView.cellForItem(at: indexPath) as? CardCollectionViewCell {
+            
+            let card = cardArray[indexPath.row]
+            if card.isFlipped == false {
+                cell.flip()
+                
+            }
+            else {
+                cell.flipback()
+            }
+            card.isFlipped = !card.isFlipped
+            
+            
+        }
+    }
+    
+}
