8dd5c3ab75294d6467313de7cce6b2f1e2e1b914
Aleksander <aleksanderaz@gmail.com>
2020-01-15 11:54:56 +0200

    start

---
+++ LoginScreen/LoginScreen/LoginScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginScreen
+//
+//  Created by Z on 1/15/20.
+//  Copyright © 2020 Zyma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
